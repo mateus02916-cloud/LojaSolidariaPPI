432de3a0b2eec77be69b8485d430d98b33620cef
mateus02916-cloud <mateus.02916@aluno.iffar.edu.br>
2025-12-16 21:42:26 -0300

    Adicionando primeiros arquivos
    Agora vai Clara

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+ // [RF 001 cadastro de intes 
+    public static void main (String[] args){
+        System.out.println("Selecione uma opção:");
+
+        //mudo
+
+        
+
+
+    }
+    
+    
+}
