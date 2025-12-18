41a04f119aa66d4720196b54b8965343531b308f
mateus02916-cloud <mateus.02916@aluno.iffar.edu.br>
2025-12-17 22:18:31 -0300

    Terminando classe menu
    Terminando classe menu para usar

--- Menu.java
+++ Menu.java
@@ -33,9 +33,93 @@ public class Menu {
                 case 1:
                     estoque.exibirEstoque();
                     break;
+
+                case 2:
+                    menuAdicionar();
+                break;
+
+                case 3:
+                    menuRemover();
+                break;
+
+                case 4:
+                    estoque.listarCategorias();
+                break;
+
+                case 0:
+                    System.out.println("Saindo do sistema...");
+                
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+                
             }
+
+            if (opcao != 0){
+                System.out.println("Precine Enter para continuar...");
+                scanner.nextLine();
+                
+
+            }
+
             
-        }
+        } while (opcao != 0);
+        scanner.close();
 
 
 }
+
+private void menuAdicionar(){
+    System.out.println("\n=== ADICIONAR QUANTIDADE ===");
+    estoque.listarCategorias();
+
+    System.out.println("Digite o nome da categoria: ");
+    String categoria = scanner.nextLine();
+
+    if (!estoque.categoriaExiste(categoria)){
+        System.out.println("Categoria inválida!");
+        return;
+    }
+
+    System.out.println("Digite a quantidade a dicionar: ");
+    int quantidade = scanner.nextInt();
+    scanner.nextLine();  //Limpar buffer
+
+    if (quantidade <= 0){
+        System.out.println("Quantidade deve ser maior que zero!");
+        return;
+
+    }
+
+    estoque.adicionarQuantidade(categoria, quantidade);
+
+
+}
+
+    private void menuRemover(){
+        System.out.println("\n=== REMOVER QUANTIDADE ===");
+        estoque.listarCategoria();
+
+        System.out.println("Digite o nome da categoria:");
+        String categora = scanner.nextLine();
+
+        if (!estoque.categoriaExiste(categoria)){
+            System.out.println("Categoria inválida!");
+            return;
+
+        }
+
+        System.out.println("Digitge a quantidade a remover: ");
+        int quantidade = scanner.nextInt();
+        scanner.nextLine(); // Limpar buffer
+
+        if (quantidade <0){
+            System.out.println("Quantidade deve ser maior que zero!");
+            return
+        }
+
+        estoque.removerQuantidade(categoria, quantidade);
+        
+
+    }
+
+
