5b6e89da5c1a4839e8f2b04289123f8ea6c78294
Clara Valença <clara.06840@aluno.iffar.edu.br>
2025-12-23 10:28:31 -0300

    fix: corrigindo fechamento de chaves
    Arrumando classe Estoque

--- Estoque.java
+++ Estoque.java
@@ -7,8 +7,9 @@ public class Estoque {
         private static String ARQUIVO_CSV ="estoque.csv";
         private static String [] CATEGORIAS = { "Masculinos","Femininos","Infantil","Calçados","Diversos"};
     
-    
-
+    public Estoque() {
+        itens = new ArrayList<>();  
+    }
 
     private void lerEstoque(){
         try (BufferedReader br = new BufferedReader(new FileReader(ARQUIVO_CSV))){
@@ -24,28 +25,27 @@ public class Estoque {
                     itens.add(new Itens(categoria, quantidade));
                 }
 
+            }
+        } catch (IOException e){
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+
+    }
     public void salvarEstoque(){
         try (PrintWriter pw = new PrintWriter(new FileWriter(ARQUIVO_CSV))){
             
-            for (Itens intes : itens){
+            for (Itens itens : itens){
                 pw.println(itens.toString());
             }
 
-
-        } catch (IOExeption e){
+        } catch (IOException e){
             System.out.println("Erro ao salver o arquivo" + e.getMessage());
 
         }
 
 
-
     }
   
-
-    }
-        }
-
-    }
 
 }
 
