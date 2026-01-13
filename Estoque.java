83da16759bfec2966e475027636a4bce0f289788
mateus02916-cloud <mateus.02916@aluno.iffar.edu.br>
2026-01-13 08:57:13 -0300

    Revert "feat: adicionando método para datas"
    This reverts commit 9a08d4fae19955351a8fbf9f00a7f000477c53dc.

--- Estoque.java
+++ Estoque.java
@@ -2,19 +2,15 @@ import java.io.*;
 import java.util.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class Estoque {
     private List<Itens> itens;
     private static final String ARQUIVO_CSV = "Estoque.csv";
+    private static final String[] CATEGORIAS = { "Masculinos", "Femininos", "Infantil", "Calçados", "Diversos" };
 
-    private List<Retiradas> retiradas;
+    private List<Itens> dados;
     private static final String RELATORIO_CSV = "Relatorio.csv";
 
-    private static final String RetiradasPorDia_CSV = "RetiradasPorDia.csv";
-    private List<RetiradasPorDia> retiradasPorDia;
-
-    private static final String[] CATEGORIAS = { "Masculinos", "Femininos", "Infantil", "Calçados", "Diversos" };
     
     public Estoque() {
         itens = new ArrayList<>();
@@ -116,52 +112,17 @@ public class Estoque {
     }
 
 
-    public boolean comparaData(List<Retiradas> retiradas) {
-        int totalRetiradasDia = 0;
-        int temp = 0;
-        String data = String.valueOf(retiradas.get(0)); 
-        String[] partes = data.split("/");             
-        String dia = partes[0];   
-        
-        for (Retiradas retirada : retiradas) {
-            int i = 1;
-            while (i <=31) {
-                if (dia.equals(String.valueOf(i))){
-                    temp = totalRetiradasDia += totalRetiradasDia;
-
-                    totalRetiradasDia = temp;
-
-                    retiradasPorDia.add(new retiradasPorDia(totalRetiradasDia));
-                }
-            }
-            
-        }
-
-        //percorre a lista de retirada e compara as datas
+    public double comparaData(){
+        //percorre a lista de dados e compara as datas
         //tera que ter um contador de entradas
     }
 
-    public void getData(String opcaoData) {
-        LocalDate capturaData;
-        DateTimeFormatter formata = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        if (opcaoData.equals("1")) {
-            capturaData = LocalDate.now();
-            String data = capturaData.format(formata);
-            
-        }  else {
-
-            try {
-                capturaData = LocalDate.parse(opcaoData, formata);
-                String data = capturaData.format(formata);
-            } catch (DateTimeParseException e) {
-                System.out.println("A data '" + opcaoData + "' NÃO é válida no formato dd/MM/yyyy.");
-            }
-        }
-
+    public void getData() {
+        LocalDate data = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        data.format(formatter);
     }
 
-    
 
     public void gerarRelatorioMensal(int mes, int ano) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -169,19 +130,17 @@ public class Estoque {
         System.out.println("\n=== RELATÓRIO DE ESTOQUE - " + mesAno + " ===");
         exibirEstoque();
 
-        
 
         File relatorio = new File(RELATORIO_CSV);
         try (BufferedReader br = new BufferedReader(new FileReader(RELATORIO_CSV))) {
             String linha;
             while ((linha = br.readLine()) != null) {
-                String[] retirada = linha.split(",");
+                String[] dados = linha.split(",");
 
-                if (retirada.length == 3) {
-                    String dia = retirada[0].trim();
-                    int itens_saida = Integer.parseInt(retirada[1].trim());
-                    itens.add(new Itens(dia, itens_saida));
-
+                if (dados.length == 3) {
+                    String categoria = dados[0].trim();
+                    int quantidade = Integer.parseInt(dados[1].trim());
+                    itens.add(new Itens(categoria, quantidade));
                 }
             }
         }
@@ -190,16 +149,5 @@ public class Estoque {
             System.out.println("Erro ao carregar dados do relatório: " + e.getMessage());
         }
     }
-
-    public void salvarRelatorio() {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(RELATORIO_CSV))) {
-            for (Itens item : itens) {
-                pw.println(item.toString());
-            }
-        } catch (IOException e) {
-            System.out.println("Erro ao salvar arquivo: " + e.getMessage()); 
-        }
-    }
-
     
 }
--- teste.java
+++ teste.java
@@ -1,49 +1,4 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.*;
 
-public class teste {
- 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        String opcaoData = sc.nextLine();
-
-
-        LocalDate capturaData;
-        DateTimeFormatter formata = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        if (opcaoData.equals("1")) {
-            capturaData = LocalDate.now();
-            String data = capturaData.format(formata);
-            
-        }  else {
-
-            try {
-                capturaData = LocalDate.parse(opcaoData, formata);
-                String data = capturaData.format(formata);
-            } catch (DateTimeParseException e) {
-                System.out.println("A data '" + data + "' NÃO é válida no formato dd/MM/yyyy.");
-            }
-        }
-
-        System.out.println(data);
-        System.out.println(data.getClass().getSimpleName());
-
-
-       /*  LocalDate data = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-
-        System.out.println(data.format(formatter));////
-       //System.out.println("teste 2 :" + data.format(formatter).getClass().getSimpleName());  */
-       
-
-       
-    }
-}
 
 
 
