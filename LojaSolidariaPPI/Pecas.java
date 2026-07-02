615732db5e78e3c4bec39543e5628d4560e602c9
Clara Valença <clara.06840@aluno.iffar.edu.br>
2026-07-02 12:08:30 -0300

    feat : criação getters e setters da classe Pecas

--- LojaSolidariaPPI/Pecas.java
+++ LojaSolidariaPPI/Pecas.java
@@ -3,4 +3,28 @@ public class Pecas {
     private int id;
     private int quantidade;
     private static String[] CATEGORIAS = { "Masculinos", "Femininos", "Infantil", "Calçados", "Diversos" };
+
+    public int getId() {
+        return id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public static String[] getCATEGORIAS() {
+        return CATEGORIAS;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public static void setCATEGORIAS(String[] cATEGORIAS) {
+        CATEGORIAS = cATEGORIAS;
+    }
 }
